03b6b05823725071520ae294285ed4898ba68652
fengchangJ <40262797+fengchangJ@users.noreply.github.com>
2019-03-18 12:16:42 +0800

    Update MainClient.java

--- src/com/atguigu/client/MainClient.java
+++ src/com/atguigu/client/MainClient.java
@@ -6,6 +6,7 @@ import com.atguigu.webservice.HelloWorldImplService;
 public class MainClient {
 
 	public static void main(String[] args) {
+		//
 		HelloWorld helloWorld = new HelloWorldImplService().getHelloWorldImplPort();
 		System.out.println(helloWorld.sayHello("nihao", 18));
 
